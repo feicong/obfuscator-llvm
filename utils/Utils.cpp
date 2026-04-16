0ef70341f08788c32866d7e843225808e5345c75
feicong <fei_cong@hotmail.com>
2026-04-16 18:40:08 +0800

    兼容新版本LLVM并新增Linux和macOS发布CI

--- utils/Utils.cpp
+++ utils/Utils.cpp
@@ -50,7 +50,12 @@ void fixStack(Function *f) {
     }
 
     for (unsigned int i = 0; i != tmpPhi.size(); ++i) {
-      DemotePHIToStack(tmpPhi.at(i), f->begin()->getTerminator()->getIterator());
+#if LLVM_VERSION_MAJOR >= 19
+      DemotePHIToStack(tmpPhi.at(i),
+                       f->begin()->getTerminator()->getIterator());
+#else
+      DemotePHIToStack(tmpPhi.at(i), f->begin()->getTerminator());
+#endif
     }
 
   } while (tmpReg.size() != 0 || tmpPhi.size() != 0);
